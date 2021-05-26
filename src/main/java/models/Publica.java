037d0bc2ce65f1234b954016cf8cb73b23dde362
Alan <alan.kalbermatter.dev@gmail.com>
2021-05-26 18:54:06 -0300

    main update

---
+++ src/main/java/models/Publica.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Publica {
+    private String nombre;
+
+    public Publica(){}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
