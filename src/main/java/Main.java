608fcaeb38088426e111c1c5b02dc0166ee916bc
Alan <alan.kalbermatter.dev@gmail.com>
2021-05-27 19:08:13 -0300

    new update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,20 +18,28 @@ public class Main {
         Random random = new Random();
 
         int i = 0;
+        int ip = 0;
         int j = 0;
         int matricula = 100;
         int tipoNave = 0;
+        int o = 0;
+        int op = 0;
+        int b = 1;
+        int agenPri = 0;
+//        //crear orbita
+//        Orbita orbita = new Orbita();
+//        double ran1 = random.nextDouble();
+//        if (ran1 < 0 )
+//            ran1 = ran1 * -1;
+//        orbita.setCoordR(ran1);
+//        double ran2 = random.nextDouble();
+//        if (ran2 < 0)
+//            ran2 = ran2 * -1;
+//        orbita.setCoordFi(ran2);
 
-        //crear orbita
-        Orbita orbita = new Orbita();
-        double ran1 = random.nextDouble();
-        if (ran1 < 0 )
-            ran1 = ran1 * -1;
-        orbita.setCoordR(ran1);
-        double ran2 = random.nextDouble();
-        if (ran2 < 0)
-            ran2 = ran2 * -1;
-        orbita.setCoordFi(ran2);
+
+
+
 
         //loop principal que crea agencias publicas
         while( j != 7 ) {
@@ -42,18 +50,54 @@ public class Main {
 
             //loop que crea tipos de naves publicas
             while(tipoNave != 10) {
+
+                //crear tipo nave
+                TipoNave tN = new TipoNave();
+                int ran3 = random.nextInt();
+                if (ran3 < 0 )
+                    ran3 = ran3 * -1;
+                tN.setCod(ran3);
                 //crear naves publicas
                 while (i != 130){
                     NaveEspacial nave = new NaveEspacial();
                     nave.setMission(faker.space().star());
                     nave.setMatricula(matricula++);
                     nave.setAgencia(aPub.getNombre());
+
                     //crear basura publica
+                    while(o != 1000){
+                        //crear orbita
+                        Orbita orbita = new Orbita();
+                        double ran1 = random.nextDouble();
+                        if (ran1 < 0 )
+                            ran1 = ran1 * -1;
+                        orbita.setCoordR(ran1);
+                        double ran2 = random.nextDouble();
+                        if (ran2 < 0)
+                            ran2 = ran2 * -1;
+                        orbita.setCoordFi(ran2);
+                        //crear basura
+                        Basura basura = new Basura();
+                        basura.setVelocity(random.nextDouble());
+                        basura.setCod(b);
+                        basura.setCoordFi(orbita.getCoordFi());
+                        basura.setCoordR(orbita.getCoordR());
+                        double ran6 = random.nextDouble();
+                        if (ran6 < 0)
+                            ran6 = ran6 * -1;
+                        basura.setSize(ran6);
+                        double ran7 = random.nextDouble();
+                        if (ran7 < 0)
+                            ran7 = ran7 * -1;
+                        basura.setWeight(ran7);
+                        b++;
+                    }
                 }
             }
 
             //loop que crea agencias privadas
-            while(j != 24){
+            tipoNave = 0;
+            while(agenPri != 24){
                 Agencia aPriv = new Agencia();
                 Privada privada = new Privada();
                 aPriv.setNombre(faker.space().company());
@@ -64,15 +108,49 @@ public class Main {
                 while(tipoNave != 10) {
 
                     //crear naves privadas
-                    while (i != 460){
+                    while (ip != 460){
                         NaveEspacial nave = new NaveEspacial();
                         nave.setMission(faker.space().star());
                         nave.setMatricula(matricula++);
                         nave.setAgencia(aPriv.getNombre());
                         //crear basura privada
+
+                        while(op != 100){
+                            //crear orbita
+                            Orbita orbita = new Orbita();
+                            double ran1 = random.nextDouble();
+                            if (ran1 < 0 )
+                                ran1 = ran1 * -1;
+                            orbita.setCoordR(ran1);
+                            double ran2 = random.nextDouble();
+                            if (ran2 < 0)
+                                ran2 = ran2 * -1;
+                            orbita.setCoordFi(ran2);
+                            //crear basura
+                            Basura basura = new Basura();
+                            basura.setVelocity(random.nextDouble());
+                            basura.setCod(b);
+                            b++;
+                            basura.setCoordFi(orbita.getCoordFi());
+                            basura.setCoordR(orbita.getCoordR());
+                            double ran6 = random.nextDouble();
+                            if (ran6 < 0)
+                                ran6 = ran6 * -1;
+                            basura.setSize(ran6);
+                            double ran7 = random.nextDouble();
+                            if (ran7 < 0)
+                                ran7 = ran7 * -1;
+                            basura.setWeight(ran7);
+
+                            op++;
+                        }
+                        ip++;
                     }
+                    tipoNave++;
                 }
+                agenPri++;
             }
+            j++;
         }
 
         //crear tipo de nave
